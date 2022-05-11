eae169c60a575f8358277d4d1986fe1f7eb0135c
Максим Капустин <maximkapus7in@yandex.ru>
2022-05-11 11:42:55 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.add(8);
-        tree.add(9);
-        tree.add(5);
-        System.out.println(tree.findSubtrees());
+        
     }
 }
